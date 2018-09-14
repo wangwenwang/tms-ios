edd68215b59ea57e409d357ef3352795128e4a73
wangww <372266373@qq.com>
2018-09-14 18:17:24 +0800

    添加微信登录功能

--- tms-ios/AppDelegate.m
+++ tms-ios/AppDelegate.m
@@ -7,8 +7,15 @@
 //
 
 #import "AppDelegate.h"
+#import <WXApi.h>
+#import <AFNetworking.h>
+#import "NSString+toDict.h"
+#import "NSDictionary+toString.h"
+#import "ViewController.h"
 
-@interface AppDelegate ()
+@interface AppDelegate ()<WXApiDelegate>
+
+@property (weak, nonatomic) UIWebView *webView;
 
 @end
 
@@ -17,7 +24,18 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    
+    [WXApi registerApp:@"wx4c368e3f56d8ace2"];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(receiveWebView:) name:kReceive_WebView_Notification object:nil];
+    
     return YES;
+}
+
+
+- (void)receiveWebView:(NSNotification *)aNotification {
+    
+    _webView = aNotification.userInfo[@"webView"];
 }
 
 
@@ -47,5 +65,124 @@
     // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
 }
 
+- (BOOL)application:(UIApplication *)application handleOpenURL:(NSURL *)url {
+    
+    return [WXApi handleOpenURL:url delegate:self];
+}
+
+- (BOOL)application:(UIApplication *)application openURL:(NSURL *)url sourceApplication:(NSString *)sourceApplication annotation:(id)annotation {
+    
+    return [WXApi handleOpenURL:url delegate:self];
+}
+
+// 授权回调的结果
+- (void)onResp:(BaseResp *)resp {
+    
+    NSLog(@"resp:%d", resp.errCode);
+    
+    if([resp isKindOfClass:[SendAuthResp class]]) {
+        
+        SendAuthResp *rep = (SendAuthResp *)resp;
+        if(resp.errCode == -2) {
+            
+            NSLog(@"用户取消");
+        }else if(resp.errCode == -4) {
+            
+            NSLog(@"用户拒绝授权");
+        }else {
+            
+            NSString *code = rep.code;
+            NSString *appid = @"wx4c368e3f56d8ace2";
+            NSString *appsecret = @"f8faea84b624079c51d59b42185bae31";
+            NSString *url = [NSString stringWithFormat:@"https://api.weixin.qq.com/sns/oauth2/access_token?appid=%@&secret=%@&code=%@&grant_type=authorization_code", appid, appsecret, code];
+            
+            AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+            manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+            [manager GET:url parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+            } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+                
+                NSDictionary *result = [[[ NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding] toDict];
+                NSString *access_token = result[@"access_token"];
+                NSString *openid = result[@"openid"];
+                [self wxLogin:access_token andOpenid:openid];
+                NSLog(@"请求access_token成功");
+                [self bindingWX:openid];
+                
+            } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+                
+                NSLog(@"请求access_token失败");
+            }];
+        }
+    }
+}
+
+
+// 获取tms用户信息
+- (void)bindingWX:(NSString *)openid {
+    
+    NSString *params = [NSString stringWithFormat:@"{\"wxOpenid\":\"%@\"}", openid];
+    NSString *paramsEncoding = [params stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+    NSString *url = [NSString stringWithFormat:@"http://zwlttest.3322.org:8081/tmsApp/login.do?params=%@", paramsEncoding];
+    NSLog(@"请求tms用户信息参数：%@",url);
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager GET:url parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSDictionary *result = [[[NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding] toDict];
+        
+        int status = [result[@"status"] intValue];
+        id data = result[@"data"];
+        NSString *Msg = result[@"Msg"];
+        
+        if(status == 1) {
+            
+            NSString *params = [result toString];
+            NSString *paramsEncoding = [params stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+            NSString *jsStr = [NSString stringWithFormat:@"WXBind_YES_Ajax('%@')", paramsEncoding];
+            NSLog(@"%@",jsStr);
+            [_webView stringByEvaluatingJavaScriptFromString:jsStr];
+            NSLog(@"请求tms用户信息成功");
+        } else if(status == 3){
+            
+            if([data isKindOfClass:[NSString class]]) {
+                
+                NSString *jsStr = [NSString stringWithFormat:@"WXBind_NO_Ajax('%@')",openid];
+                NSLog(@"%@",jsStr);
+                [_webView stringByEvaluatingJavaScriptFromString:jsStr];
+                NSLog(@"些微信未注册");
+            }
+        }else {
+            
+            NSLog(@"%@", Msg);
+        }
+        NSLog(@"%@", result);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        NSLog(@"请求wms用户信息失败");
+    }];
+}
+
+
+// 获取微信个人信息
+- (void)wxLogin:(NSString *)access_token andOpenid:(NSString *)openid {
+    
+    NSString *url = [NSString stringWithFormat:@"https://api.weixin.qq.com/sns/userinfo?access_token=%@&openid=%@", access_token, openid];
+    
+    AFHTTPSessionManager *manager = [AFHTTPSessionManager manager];
+    manager.responseSerializer = [AFHTTPResponseSerializer serializer];
+    [manager GET:url parameters:nil progress:^(NSProgress * _Nonnull downloadProgress) {
+    } success:^(NSURLSessionDataTask * _Nonnull task, id  _Nullable responseObject) {
+        
+        NSDictionary *result =[[[ NSString alloc] initWithData:responseObject encoding:NSUTF8StringEncoding] toDict];
+        NSLog(@"请求个人信息成功");
+        NSLog(@"%@", result);
+        
+    } failure:^(NSURLSessionDataTask * _Nullable task, NSError * _Nonnull error) {
+        
+        NSLog(@"请求个人信息失败");
+    }];
+}
 
 @end
--- tms-ios/ViewController.m
+++ tms-ios/ViewController.m
@@ -8,6 +8,8 @@
 
 #import "ViewController.h"
 #import <ZipArchive.h>
+#import <WXApi.h>
+#import <JavaScriptCore/JavaScriptCore.h>
 
 @interface ViewController () {
     
@@ -35,16 +37,105 @@
     NSString *filePath = [NSString stringWithFormat:@"%@/dist/%@", unzipPath, @"index.html"];
     NSURL *url = [[NSURL alloc] initWithString:filePath];
     [_webView loadRequest:[NSURLRequest requestWithURL:url]];
+    _request = [NSURLRequest requestWithURL:url];
     
-//    NSURL *url = [NSURL URLWithString:@"https://www.baidu.com"];
-//    NSURLRequest *request = [NSURLRequest requestWithURL:url];
-//    [_webView loadRequest:request];
+    _webView.autoresizingMask = UIViewAutoresizingFlexibleHeight | UIViewAutoresizingFlexibleWidth;
+    [[NSNotificationCenter defaultCenter] postNotificationName:kReceive_WebView_Notification object:nil userInfo:@{@"webView":_webView}];
+    
+    if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"weixin://"]] || [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"Whatapp://"]] || [WXApi isWXAppInstalled]) {
+        
+        //微信
+        NSLog(@"YESWX");
+    }else {
+        
+        // 移除微信按钮
+        dispatch_async(dispatch_get_global_queue(0, 0), ^{
+            
+            for (int i = 0; i < 20; i++) {
+                
+                dispatch_async(dispatch_get_main_queue(), ^{
+                    
+                    NSString *jsStr = [NSString stringWithFormat:@"WXInstall_Check_Ajax('%@')", @"NO"];
+                    NSLog(@"%@",jsStr);
+                    [_webView stringByEvaluatingJavaScriptFromString:jsStr];
+                });
+                usleep(100000);
+            }
+        });
+    }
+    
+    // 显示版本号
+    dispatch_async(dispatch_get_global_queue(0, 0), ^{
+        
+        for (int i = 0; i < 20; i++) {
+            
+            dispatch_async(dispatch_get_main_queue(), ^{
+                
+                NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+                NSString *app_Version = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
+                NSString *jsStrVersion = [NSString stringWithFormat:@"VersionShow('版本:%@')", app_Version];
+                NSLog(@"%@",jsStrVersion);
+                [_webView stringByEvaluatingJavaScriptFromString:jsStrVersion];
+            });
+            usleep(100000);
+        }
+    });
+    
+    
+    NSString *jsStr = [NSString stringWithFormat:@"QRScanAjax('%@', '%@', '%@', '%@',)" ,@"1" ,@"2" ,@"3" ,@"4"];
+    NSLog(@"%@",jsStr);
+    [_webView stringByEvaluatingJavaScriptFromString:jsStr];
 }
 
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)webViewDidFinishLoad:(UIWebView *)webView{
+    // iOS监听vue的函数
+    JSContext *context = [self.webView valueForKeyPath:@"documentView.webView.mainFrame.javaScriptContext"];
+    context[@"CallAndroidOrIOS"] = ^() {
+        NSString * qrscanDes = @"";
+        NSArray *args = [JSContext currentArguments];
+        for (JSValue *jsVal in args) {
+            qrscanDes = jsVal.toString;
+            break;
+        }
+        
+        if([qrscanDes isEqualToString:@"微信登录"]) {
+            
+            SendAuthReq* req = [[SendAuthReq alloc] init];
+            req.scope = @"snsapi_userinfo";
+            req.state = @"wechat_sdk_tms";
+            dispatch_async(dispatch_get_main_queue(), ^{
+                [WXApi sendReq:req];
+            });
+        }
+        // 第一次加载登录页，不执行此函数，所以还写了一个定时器
+        else if([qrscanDes isEqualToString:@"登录页面已加载"]) {
+            
+            if ([[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"weixin://"]] || [[UIApplication sharedApplication] canOpenURL:[NSURL URLWithString:@"Whatapp://"]] || [WXApi isWXAppInstalled]) {
+                
+                //微信
+                NSLog(@"YESWX");
+            }else {
+                
+                // 移除微信按钮
+                NSString *jsStr = [NSString stringWithFormat:@"WXInstall_Check_Ajax('%@')", @"NO"];
+                NSLog(@"%@",jsStr);
+                [_webView stringByEvaluatingJavaScriptFromString:jsStr];
+            }
+            
+            NSDictionary *infoDictionary = [[NSBundle mainBundle] infoDictionary];
+            NSString *app_Version = [infoDictionary objectForKey:@"CFBundleShortVersionString"];
+            NSString *jsStrVersion = [NSString stringWithFormat:@"VersionShow('版本:%@')", app_Version];
+            NSLog(@"%@",jsStrVersion);
+            [_webView stringByEvaluatingJavaScriptFromString:jsStrVersion];
+        }
+        NSLog(@"%@",qrscanDes);
+    };
 }
 
 
